7add64bb96232adafa8525e1ef2cdbb3b4d840ff
Antonio Mendiola <arm160230@utdallas.edu>
2019-04-08 18:43:05 -0500

    Reorganized project structure and added test skeleton for ShoppingCart

---
+++ test/ShoppingCartTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShoppingCartTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getBalance() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void getItemCount() {
+    }
+
+    @Test
+    void empty() {
+    }
+}
